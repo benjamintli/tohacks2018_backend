0e29b8bd289765e4e362becb1f461eabd7ab3bbf
benjamintli <15btl1@queensu.ca>
2018-05-26 13:28:29 -0400

    added python files

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+import requests, json
+
+
+app = Flask(__name__)
+
+url1 = "https://maps.googleapis.com/maps/api/place/details/json?placeid="
+url2 = "&key="
+api_key = "AIzaSyDdxOnExyqXGzl36dbk41gbrIZcpRQ037s"
+
+
+@app.route("/reviews", methods=["POST"])
+def get_reviews():
+    if request.method == "POST":
+        req = request.get_json(force=True)
+        url = url1 + req['placeID']+ url2 + api_key
+        r = requests.get(url)
+        json_dict = r.json()
+        return jsonify(json_dict['result']['reviews'])
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+
+if __name__ == "__main__":
+    app.run()
